f8a4227940c8bc2e27b52d3fe7c0b3cf0b763d8d
Ray Hsiao <40257341+rh5140@users.noreply.github.com>
2021-12-13 09:44:13 -0800

    Projectile collider exists at all

--- Assets/Scripts/PointAndShoot.cs
+++ Assets/Scripts/PointAndShoot.cs
@@ -8,7 +8,6 @@ using UnityEngine;
 public class PointAndShoot : MonoBehaviour
 {
     public GameObject player;
-    // public GameObject projectile;
     public GameObject projectilePrefab;
     public GameObject crosshairs;
 
@@ -16,29 +15,36 @@ public class PointAndShoot : MonoBehaviour
     private Vector3 cursorPosition;
     private Vector3 targetPosition;
     private bool tongueOut;
+    private bool hitBug;
     private float duration;
 
     public float projectileSpeed = 20.0f;
+
+    public Projectile projectile;
 
     // Start is called before the first frame update
     void Start()
     {
         // Cursor.visible = false;
-        // startPosition = projectile.transform.position;
         tongueOut = false;
+        // Projectile projectile = gameObject.GetComponent<Projectile>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Restrict cursor position too
         cursorPosition = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
         crosshairs.transform.position = new Vector2(cursorPosition.x, cursorPosition.y);
 
         // Rotate chameleon head to face cursor
-        // Need to limit rotation since chameleon can't turn head all the way around...
         Vector3 difference = cursorPosition - player.transform.position;
         float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        player.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+        rotationZ = Mathf.Clamp (rotationZ, -60, 60);
+        if (!tongueOut)
+        {
+            player.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+        }
         
         if (Input.GetMouseButtonDown(0))
         {
@@ -50,7 +56,7 @@ public class PointAndShoot : MonoBehaviour
             duration = distance / projectileSpeed;
             Vector2 direction = difference / distance;
             direction.Normalize();
-            if (tongueOut == false)
+            if (!tongueOut)
             {
                 StartCoroutine(Tongue(direction, rotationZ));
             }
@@ -60,10 +66,9 @@ public class PointAndShoot : MonoBehaviour
     {
         tongueOut = true;
 
-        // Replace w/ persistent projectile later
-        GameObject b = Instantiate(projectilePrefab) as GameObject;
-        b.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
-        b.GetComponent<Rigidbody2D>().velocity = direction * projectileSpeed;
+        // TO DO: object pooling w/ this one tongue projectile
+        projectile.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+        projectile.GetComponent<Rigidbody2D>().velocity = direction * projectileSpeed;
 
         startPosition = projectilePrefab.transform.position;
         float time = 0;
@@ -71,24 +76,23 @@ public class PointAndShoot : MonoBehaviour
         // Lerp between spawn point and click point
         while (time < duration)
         {
-            b.transform.position = Vector3.Lerp(startPosition, targetPosition, time / duration);
+            projectile.transform.position = Vector3.Lerp(startPosition, targetPosition, time / duration);
             time += Time.deltaTime;
-            // If collide, break
             yield return null;
         }
         
-        b.transform.position = targetPosition;
+        // Get position again in case hit bug before click point
+        projectile.transform.position = targetPosition;
         // Lerp back to spawn point
         time = 0;
         while (time < duration)
         {
-            b.transform.position = Vector3.Lerp(targetPosition, startPosition, time / duration);
+            projectile.transform.position = Vector3.Lerp(targetPosition, startPosition, time / duration);
             time += Time.deltaTime;
             yield return null;
         }    
 
-        // Won't be necessary later
-        Destroy(b);
+        // Destroy(b);
         tongueOut = false;
         yield return null;
     }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,91 +1,20 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class Projectile : MonoBehaviour {
-//     // public GameObject crosshairs;
-//     public GameObject player;
-//     // public GameObject bulletPrefab;
-//     // public GameObject bulletStart;
+public class Projectile : MonoBehaviour 
+{
+    public bool collidedWithBug = false;
 
-//     public float bulletSpeed = 60.0f;
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("Pre collision collidedWithBugValue = " + collidedWithBug);
 
-//     private Vector3 target;
+        collidedWithBug = true;
 
-//     // Use this for initialization
-//     void Start () {
-//         Cursor.visible = false;
-//     }
-    
-//     // Update is called once per frame
-//     void Update () {
-//         target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
-//         // crosshairs.transform.position = new Vector2(target.x, target.y);
+        Destroy(col.gameObject);
+        
+        Debug.Log("Post collision collidedWithBugValue = " + collidedWithBug);
 
-//         Vector3 difference = target - player.transform.position;
-//         float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-//         player.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
-
-//         // if(Input.GetMouseButtonDown(0)){
-//         //     float distance = difference.magnitude;
-//         //     Vector2 direction = difference / distance;
-//         //     direction.Normalize();
-//         //     fireBullet(direction, rotationZ);
-//         // }
-//     }
-//     // void fireBullet(Vector2 direction, float rotationZ){
-//     //     GameObject b = Instantiate(bulletPrefab) as GameObject;
-//     //     b.transform.position = bulletStart.transform.position;
-//     //     b.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
-//     //     b.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
-//     // }
-// }
-
-
-// // using System.Collections;
-// // using System.Collections.Generic;
-// // using UnityEngine;
-
-// // public class Projectile : MonoBehaviour
-// // {
-// //     public GameObject player;
-// //     public GameObject target;
-
-// //     public float speed = 10f;
-
-// //     public Vector3 movePosition;
-
-// //     private float playerX;
-// //     private float targetX;
-// //     private float nextX;
-// //     private float dist;
-// //     private float baseY;
-// //     private float height;
-
-// //     // Start is called before the first frame update
-// //     void Start()
-// //     {
-// //         player = GameObject.FindGameObjectWithTag("Player");
-// //         target = GameObject.FindGameObjectWithTag("Enemy");
-// //     }
-
-// //     // Update is called once per frame
-// //     void Update()
-// //     {
-// //         playerX = player.transform.position.x;
-// //         targetX = target.transform.position.x;
-// //         dist = targetX - playerX;
-// //         nextX = Mathf.MoveTowards(transform.position.x, targetX, speed * Time.deltaTime);
-// //         baseY = Mathf.Lerp(player.transform.position.y, target.transform.position.y, (nextX - playerX) / dist);
-// //         height = 2 * (nextX - playerX) * (nextX - targetX) / (-0.25f * dist * dist);
-
-// //         // movePosition = new Vector2(nextX, baseY + height);
-
-// //         transform.position = movePosition;
-
-// //         if (movePosition == target.transform.position)
-// //         {
-// //             Destroy(gameObject);
-// //         }
-// //     }
-// // }
+    }
+}
